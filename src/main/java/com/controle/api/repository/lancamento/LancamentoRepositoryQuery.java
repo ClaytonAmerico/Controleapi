7c60c0cad3f9a89029260f937ab2fcc35494833a
Clayton <clayton.americo@gmail.com>
2019-12-29 10:18:43 -0300

    Implements projection

--- src/main/java/com/controle/api/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/com/controle/api/repository/lancamento/LancamentoRepositoryQuery.java
@@ -5,9 +5,11 @@ import org.springframework.data.domain.Page;
 
 import com.controle.api.model.Lancamento;
 import com.controle.api.repository.filter.LancamentoFilter;
+import com.controle.api.repository.projection.ResumoLancamento;
 
 public interface LancamentoRepositoryQuery {
 	
 	public Page<Lancamento> filtrar(LancamentoFilter lalancamentoFilter, Pageable pageable);
+	public Page<ResumoLancamento> resumir(LancamentoFilter lalancamentoFilter, Pageable pageable);
 
 }
--- src/main/java/com/controle/api/resource/LancamentoResource.java
+++ src/main/java/com/controle/api/resource/LancamentoResource.java
@@ -30,6 +30,7 @@ import com.controle.api.exception.ControleapiExceptionHandler.Erro;
 import com.controle.api.model.Lancamento;
 import com.controle.api.repository.LancamentoRepository;
 import com.controle.api.repository.filter.LancamentoFilter;
+import com.controle.api.repository.projection.ResumoLancamento;
 import com.controle.api.service.LancamentoService;
 import com.controle.api.service.exception.PessoaInexistenteOuInativaException;
 
@@ -53,6 +54,12 @@ public class LancamentoResource {
 	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
 	public Page<Lancamento> pesquisar(LancamentoFilter lancamentoFilter, Pageable pageable ) {
 		return lancamentoRepository.filtrar(lancamentoFilter, pageable);
+	}
+	
+	@GetMapping(params = "resumo")
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
+	public Page<ResumoLancamento> resumir(LancamentoFilter lancamentoFilter, Pageable pageable ) {
+		return lancamentoRepository.resumir(lancamentoFilter, pageable);
 	}
 	
 	@GetMapping("/{codigo}")
